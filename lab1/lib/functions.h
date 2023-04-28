800cefe35cfe340d78eac9284f8b34f1b7d20035
Gorgeren <73250229+Gorgeren@users.noreply.github.com>
2023-04-28 20:34:25 +0300

    Delete lab1 directory

--- lab1/lib/functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "matrix.h"
-namespace ChmodLib {
-    matrix transpose(const matrix& mat);
-    std::pair<matrix, matrix> LU(const matrix& mat);
-    std::vector<double> solve_SLAU(const matrix &L, const matrix &U, const std::vector<double>& b);
-}
